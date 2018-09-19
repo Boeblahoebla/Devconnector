09f43e11c6f4c65553ba6b9a1d9902cad32a9813
Dzengiz Tafa <dzengiz.tafa@telenet.be>
2018-09-19 11:06:46 +0200

    Added a route to like and unlike a post -> POST like at /api/posts/like -> DELETE like at /api/posts/unlike/:id (where id is the post-id) tested with postman

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -92,6 +92,56 @@ router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, re
 });
 
 
+// @route   POST api/posts/like/:id
+// @desc    Like apost
+// @access  Private
+router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then((profile) => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    // Check to see if the user already liked the post
+                    if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+                        return res.status(400).json({alreadyliked: 'User already liked this post'});
+                    }
+
+                    // Add the user id to the likes array
+                    post.likes.unshift({ user: req.user.id });
+
+                    post.save().then(post => res.json(post));
+                })
+                .catch(err => res.json(err));
+        })
+});
+
+// @route   DELETE api/posts/unlike/:id ... :id being the "post- id", NOT the "like- id"
+// @desc    Unlike a post
+// @access  Private
+router.delete('/unlike/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then((profile) => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    // Check to see if the user already liked the post
+                    if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+                        // If not already liked
+                        return res.status(400).json({notliked: 'You have nog liked this post yet'});
+                    }
+
+                    // Get to remove index
+                    const removeIndex = post.likes.map(items => items.user.toString).indexOf(req.user.id);
+
+                    // Remove the item at the index to remove
+                    post.likes.splice(removeIndex, 1);;
+
+                    //
+                    post.save().then(post => res.json(post));
+                })
+                .catch(err => res.json({ nopost: 'There is no post by that id to unlike' }));
+        })
+});
+
+
 
 
 //////////////
