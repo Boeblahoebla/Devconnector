ad6d8606a04784bb1759d54de7f535d8d0c4d5e5
Dzengiz Tafa <dzengiz.tafa@telenet.be>
2018-09-19 10:49:09 +0200

    Added a route to delete a post -> DELETE post at /api/posts/:id tested with postman

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose');
 
 // Model
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 // Security
 const passport = require('passport');
@@ -24,6 +25,26 @@ const validatePostInput = require('../../validation/post');
 // @access  Public
 router.get('/test', (req, res) => {
     res.json({ msg: "Posts works" });
+});
+
+
+// @route   GET api/posts/
+// @desc    Get all posts
+// @access  Private
+router.get('/', (req, res) => {
+   Post.find().sort({ date: -1 }) // descending order
+       .then(posts => res.json(posts))
+       .catch(err => res.status(404).json({ nopostfound: 'No posts found'}));
+});
+
+
+// @route   GET api/posts/:id
+// @desc    Get a single post by id
+// @access  Public
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id) // descending order
+        .then(post => res.json(post))
+        .catch(err => res.status(404).json({ nopostfound: 'No post found with that ID'}));
 });
 
 
@@ -48,6 +69,30 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
 
     newPost.save().then((post) => res.json(post));
 });
+
+
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then((profile) => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    // Check for post owner
+                    if (post.user.toString() !== req.user.id) {
+                        return res.status(401).json({ notauthorized: 'User not authorized' });
+                    }
+
+                    // Delete the post
+                    post.remove().then(() => res.json({ deleted: 'succes' }));
+                })
+                .catch(err => res.json(err));
+        })
+});
+
+
+
 
 //////////////
 // Exports //
