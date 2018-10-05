db91a831c5a2e17f61b676b968ae06cef62b79fa
Dzengiz Tafa <dzengiz.tafa@telenet.be>
2018-10-05 16:33:11 +0200

    Prepared for deploy

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const morgan = require('morgan');
 
 const app = express();
 
+const path = require('path');
+
 ////////////////////////////
 // Database connectivity //
 //////////////////////////
@@ -42,6 +44,16 @@ require('./config/passport')(passport);
 app.use('/api/users', usersRoute);
 app.use('/api/profile', profileRoute);
 app.use('/api/posts', postsRoute);
+
+// Server static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 ////////////////////////////
 // Server initialisation //
