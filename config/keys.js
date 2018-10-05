ed33b701c5fa2348fc0a768d758c519b24c64596
Dzengiz Tafa <dzengiz.tafa@telenet.be>
2018-10-05 16:06:52 +0200

    Added production keys & secrets. Changed production keys from the ones for development (e.g. for github)

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://dzengiz:Pr3dat0r@ds243812.mlab.com:43812/devconnector',
-    secretOrKey: 'secretToMySuperduperAwesomeFullStackApp'
-};
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
+}
